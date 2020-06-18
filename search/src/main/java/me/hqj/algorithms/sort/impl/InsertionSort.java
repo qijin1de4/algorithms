cb1c9cc80024a0dbc05afa4bb8f0dd56e25e5010
huqijin <huqijin@jd.com>
2020-06-18 17:32:20 +0800

    insertion sort.

---
+++ search/src/main/java/me/hqj/algorithms/sort/impl/InsertionSort.java
@@ -0,0 +1,31 @@
+package me.hqj.algorithms.sort.impl;
+
+import me.hqj.algorithms.sort.Sort;
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * @description: 插入排序
+ * @author: huqijin
+ * @date: 2020-06-18 17:09
+ * @version: 1.0
+ */
+public class InsertionSort implements Sort {
+    @Override
+    public int[] sort(int[] arr) {
+        if(ArrayUtils.isNotEmpty(arr)){
+            for(int i = 0; i < arr.length; i++){
+                int j = i;
+                while(j > 0 ){
+                    int previous = j-1;
+                    if(arr[j] < arr[previous]){
+                        int tmp = arr[j];
+                        arr[j] = arr[previous];
+                        arr[previous] = tmp;
+                    }
+                    j--;
+                }
+            }
+        }
+        return arr;
+    }
+}
