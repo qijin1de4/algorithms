fad6b3604d76cce4a8b89baa17ccb8915691ade8
qijinhu <huqijin@css.com.cn>
2021-12-14 15:02:41 +0800

    排序算法
    1，修正插入排序。
    2，修正shell排序。

--- search/src/main/java/me/hqj/algorithms/sort/impl/InsertionSort.java
+++ search/src/main/java/me/hqj/algorithms/sort/impl/InsertionSort.java
@@ -5,6 +5,10 @@ import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * @description: 插入排序
+ * 它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+ * 从下标0开始，通过向左两两比较，如果左边的元素比当前元素大就交换元素位置，直到左边的元素<=当前元素为止.
+ * 因为是从下标0开始，左边的子序列一定是从小到大排好序的。
+ * 所以for循环中的while循环中，只要左边元素小于当前元素就可以返回。
  * @author: huqijin
  * @date: 2020-06-18 17:09
  * @version: 1.0
@@ -12,14 +16,11 @@ import org.apache.commons.lang3.ArrayUtils;
 public class InsertionSort implements Sort {
     @Override
     public int[] sort(int[] arr) {
-        if(ArrayUtils.isNotEmpty(arr)){
-            for(int i = 0; i < arr.length; i++){
+        if(ArrayUtils.isNotEmpty(arr)) {
+            for(int i = 1; i < arr.length; i++) {
                 int j = i;
-                while(j > 0 ){
-                    int previous = j-1;
-                    if(arr[j] < arr[previous]){
-                        Sort.swap(arr, j, previous);
-                    }
+                while(j > 0 && arr[j] < arr[j-1]) {
+                    Sort.swap(arr, j, j-1);
                     j--;
                 }
             }
--- search/src/main/java/me/hqj/algorithms/sort/impl/ShellSort.java
+++ search/src/main/java/me/hqj/algorithms/sort/impl/ShellSort.java
@@ -5,39 +5,31 @@ import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * @description: 希尔排序
+ *
+ * 希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，当增量减至 1 时，整个文件恰被分成一组，算法便终止。
+ *
+ * https://baike.baidu.com/item/%E5%B8%8C%E5%B0%94%E6%8E%92%E5%BA%8F/3229428?fr=aladdin
  * @author: huqijin
  * @date: 2020-06-20 18:54
  * @version: 1.0
  */
 public class ShellSort implements Sort {
+
     @Override
     public int[] sort(int[] arr) {
-        if(ArrayUtils.isNotEmpty(arr)){
-            for(int gap = arr.length/2; gap > 0; gap /= 2 ){
-                innerInsertionSrot(arr, gap);
+
+        if(ArrayUtils.isNotEmpty(arr)) {
+            for( int gap = arr.length /2 ; gap > 0; gap /=2) {
+                for( int i = gap; i < arr.length; i++) {
+                    int j = i;
+                    int temp = arr[i];
+                    for(; j - gap >=0 && arr[j-gap] > temp; j -= gap) {
+                        arr[j] = arr[j-gap];
+                    }
+                    arr[j] = temp;
+                }
             }
         }
         return arr;
-    }
-
-    public int[] sort(int[] arr, int[] gaps){
-        if(ArrayUtils.isNotEmpty(arr) && ArrayUtils.isNotEmpty(gaps)){
-            for(int gap : gaps){
-                innerInsertionSrot(arr, gap);
-            }
-        }
-        return arr;
-    }
-
-    private void innerInsertionSrot(int[] arr, int gap) {
-        for(int i = gap; i < arr.length; i++){
-            int j = i;
-            int current = arr[j];
-            while(j-gap >= 0 && arr[j-gap] > arr[j] ){
-                 arr[j-gap] = arr[j];
-                 j -= gap;
-            }
-            arr[j] = current;
-         }
     }
 }
